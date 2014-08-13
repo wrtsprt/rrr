1281671421a156926055de8eb5dd88f39ef2c048
Raphael Doehring <raphael.doehring@vasco.com>
2014-08-13 17:21:38 +0200

    Use callback to cleanly handle success / errors on fetching feeds.

--- app/models/feed_item_downloader.rb
+++ app/models/feed_item_downloader.rb
@@ -1,29 +1,36 @@
 class FeedItemDownloader
 
+
+
   def self.cache_feed(url)
-    raise 'feed is empty' if url.blank?
-    feed = Feedjira::Feed.fetch_and_parse(url)
+    on_success = Proc.new do |url, feed|
+      feed.entries.each do |entry|
+        feed_item = FeedItem.find_by_url(entry.url)
+        Rails.logger.debug "=> processing #{entry.url}"
+        if feed_item.nil?
+          raw_content = open(entry.url).read
+          # sanitized_content = Sanitize.fragment(raw_content, :elements => ['b a img'])
+          sanitized_content = Sanitize.fragment(raw_content,Sanitize::Config::BASIC)
 
-    feed.entries.each do |entry|
-      feed_item = FeedItem.find_by_url(entry.url)
-      Rails.logger.debug "=> processing #{entry.url}"
-      if feed_item.nil?
-        raw_content = open(entry.url).read
-        # sanitized_content = Sanitize.fragment(raw_content, :elements => ['b a img'])
-        sanitized_content = Sanitize.fragment(raw_content,Sanitize::Config::BASIC)
-
-        feed_item = FeedItem.new(
-                         feed:         url,
-                         title:        entry.title,
-                         url:          entry.url,
-                         published_at: entry.published.to_s,
-                         created_at:   Time.now.to_s,
-                         content:      raw_content,
-                         sanitized_content: sanitized_content
-                         )
-        feed_item.save
+          feed_item = FeedItem.new(
+                           feed:         url,
+                           title:        entry.title,
+                           url:          entry.url,
+                           published_at: entry.published.to_s,
+                           created_at:   Time.now.to_s,
+                           content:      raw_content,
+                           sanitized_content: sanitized_content
+                           )
+          feed_item.save
+        end
       end
     end
+
+    on_failure = Proc.new do |curl, err|
+      Rails.logger.error "Fetching the feed went wrong; #{curl}, #{err}"
+    end
+
+    Feedjira::Feed.fetch_and_parse(url, on_success: on_success, on_failure: on_failure)
   end
 
 end
