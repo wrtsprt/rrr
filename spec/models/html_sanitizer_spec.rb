ef341d553b447fbe83389b89474744b4690dc76c
Raphael Doehring <raphael.doehring@gmail.com>
2014-08-17 22:31:19 +0200

    Preparing specs to test sanitize of the html.

--- spec/models/html_sanitizer_spec.rb
+++ spec/models/html_sanitizer_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
+require 'sanitize'
 
-RSpec.describe HtmlSanitizer, :type => :model do
+RSpec.describe HtmlSanitizer do
 
   describe 'ddd' do
+
+    file = File.new(Rails.root.join('test/fixtures/heise-article.html'))
+    lines = file.readlines
+    Rails.logger.debug lines
+    lines.should_not == nil
+
+    #clean = Sanitize.document(lines.join, Sanitize::Config::RELAXED)
+    #clean.should_not == nil
 
   end
 
