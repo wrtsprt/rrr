6caf0525081a17188844c231565ced0156940eae
Raphael Doehring <raphael.doehring@gmail.com>
2014-08-19 23:24:31 +0200

    Trying the rails sanitize method as baseline cleaning html.

--- spec/models/html_sanitizer_spec.rb
+++ spec/models/html_sanitizer_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 require 'sanitize'
 
+include ActionView::Helpers::SanitizeHelper
+
 RSpec.describe HtmlSanitizer do
 
   it 'sanitizes html' do
     file = File.new(Rails.root.join('test/fixtures/heise-article.html'))
     lines = file.readlines
-    Rails.logger.debug lines
+
     expect(lines).not_to eq(nil)
-
-    #clean = Sanitize.document(lines.join, Sanitize::Config::RELAXED)
-    #expect(clean).not_to eq(nil)
-
+    sanitized = sanitize(lines.join)
+    expect(sanitized).not_to eq(nil)
   end
 
 end
