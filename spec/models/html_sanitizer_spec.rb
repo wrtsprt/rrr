437669d091042cb9712517bbb5829d1c7be80818
Raphael Doehring <raphael.doehring@gmail.com>
2014-08-28 22:49:20 +0200

    Well lets try the sanitize gem again.

--- spec/models/html_sanitizer_spec.rb
+++ spec/models/html_sanitizer_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
-require 'sanitize'
-
-include ActionView::Helpers::SanitizeHelper
 
 RSpec.describe HtmlSanitizer do
 
   it 'sanitizes html' do
-    file = File.new(Rails.root.join('test/fixtures/heise-article.html'))
-    lines = file.readlines
+    file = File.new(Rails.root.join('test/fixtures/heise-article.html'), 'rb')
+    lines = file.read.encode!('UTF-8',
+        :invalid => :replace,
+        :undef   => :replace)
 
     expect(lines).not_to eq(nil)
-    sanitized = sanitize(lines.join)
+    sanitized = Sanitize.clean(lines, :remove_contents => ['script', 'style'])
     expect(sanitized).not_to eq(nil)
+    File.open(Rails.root.join('output.txt'), 'w') do |f|
+      f.write sanitized
+    end
   end
 
 end
