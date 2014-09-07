bbca80778307c3b87b24af7699a9f59e1f1353a3
Raphael Doehring <raphael.doehring@gmail.com>
2014-09-07 18:06:10 +0200

    improve the sanitisation debugging infrastructure.

--- app/controllers/improve_sanitization_controller.rb
+++ app/controllers/improve_sanitization_controller.rb
@@ -1,5 +1,7 @@
 class ImproveSanitizationController < ApplicationController
   def show
     @item = FeedItem.first
+    @cleaned = HtmlSanitizer.clean @item.content
+    @relaxed = HtmlSanitizer.relaxed @item.content
   end
 end
--- app/models/html_sanitizer.rb
+++ app/models/html_sanitizer.rb
@@ -1,2 +1,13 @@
+require 'sanitize'
+
 class HtmlSanitizer
+
+  def self.clean(input)
+    Sanitize.clean(input, :remove_contents => ['script', 'style'])
+  end
+
+  def self.relaxed(input)
+    Sanitize.document(input, Sanitize::Config::RELAXED)
+  end
+
 end
