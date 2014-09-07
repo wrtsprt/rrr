a352fb8f37d36edb039b12bdfdc4f122853e6ff9
Raphael Doehring <raphael.doehring@gmail.com>
2014-09-07 21:59:09 +0200

    Actually step one works without all the sanitisation hassle.

--- app/models/feed_item_downloader.rb
+++ app/models/feed_item_downloader.rb
@@ -8,17 +8,13 @@ class FeedItemDownloader
         feed_item = FeedItem.find_by_url(entry.url)
         Rails.logger.debug "=> processing #{entry.url}"
         if feed_item.nil?
-          raw_content = open(entry.url).read
-          sanitized_content = HtmlSanitizer.clean(raw_content)
-
           feed_item = FeedItem.new(
                            feed:         url,
                            title:        entry.title,
                            url:          entry.url,
                            published_at: entry.published.to_s,
                            created_at:   Time.now.to_s,
-                           content:      raw_content,
-                           sanitized_content: sanitized_content
+                           content:      entry.summary
                            )
           feed_item.save
         end
