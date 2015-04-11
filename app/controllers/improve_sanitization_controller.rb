57645aedf66f2d85c192209992ed8db7e3e8e451
Raphael Doehring <raphael.doehring@vasco.com>
2015-04-11 12:04:43 +0200

    We don’t need no improvement for sanitisation at the moment.
    … this was a wrong path in the beginning.

--- app/controllers/improve_sanitization_controller.rb
+++
@@ -1,7 +0,0 @@
-class ImproveSanitizationController < ApplicationController
-  def show
-    @item = FeedItem.first
-    @cleaned = HtmlSanitizer.clean @item.content
-    @relaxed = HtmlSanitizer.relaxed @item.content
-  end
-end
