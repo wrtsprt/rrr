260231e4e1f9008471e10ddef588d381b7f6fb2d
Raphael Doehring <raphael.doehring@vasco.com>
2015-05-02 23:01:53 +0200

    Make some seeds, for offline work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+sub1 = Subscription.create name: 'heise', feed_url: 'heise.de/atom', http_url: 'heise.de'
+
+1.upto(20) do |i|
+ FeedItem.create feed: "Feed", title: "breaking news: no #{i}", url: 'http://www.heise.de', content: "This is the content no #{i}", read: false, published_at: Time.now
+end
+
