2d1cc3731d02be410e9f2b650e6b8cfb794573df
Raphael Doehring <raphael.doehring@vasco.com>
2016-08-16 18:14:32 +0200

    Put refresh on subscription.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,8 @@
 class Subscription < ActiveRecord::Base
   has_many :feed_items
   has_many :subscription_notifications, dependent: :destroy
+
+  def refresh(limit = 10)
+    FeedItemDownloader.cache_feed(self)
+  end
 end
