a4bb358771a3506ef72efbdc9e884de64fc97de4
Raphael Doehring <raphael.doehring@vasco.com>
2016-08-19 13:40:34 +0200

    Add failsafe when feed parsing goes wrong. Log to error, create notification.

--- app/models/feed_item_downloader.rb
+++ app/models/feed_item_downloader.rb
@@ -4,21 +4,34 @@ class FeedItemDownloader
     url = subscription.feed_url
 
     feed_xml = Typhoeus.get(url, followlocation: true).response_body
-    feed = Feedjira::Feed.parse feed_xml
+    begin
+      feed = Feedjira::Feed.parse feed_xml
 
-    feed.entries.each do |entry|
-      feed_item = FeedItem.find_by_url(entry.url)
-      Rails.logger.debug "=> processing #{entry.url}"
-      if feed_item.nil?
-        feed_item = FeedItem.new(
-                         subscription: subscription,
-                         title:        entry.title,
-                         url:          entry.url,
-                         published_at: entry.published.to_s,
-                         content:      entry.summary
-                         )
-        feed_item.save
+      feed.entries.each do |entry|
+        feed_item = FeedItem.find_by_url(entry.url)
+        Rails.logger.debug "=> processing #{entry.url}"
+        if feed_item.nil?
+          feed_item = FeedItem.new(
+                           subscription: subscription,
+                           title:        entry.title,
+                           url:          entry.url,
+                           published_at: entry.published.to_s,
+                           content:      entry.summary
+                           )
+          feed_item.save
+        end
       end
+    rescue Exception => e
+      Rails.logger.error "Error fetching for subscription #{subscription.id}"
+      Rails.logger.error "#{subscription.name}: #{url}"
+      Rails.logger.error e
+
+      message = "Error fetching for subscription #{subscription.id}\n"
+      message += "#{subscription.name}: #{url}\n"
+      message += "#{e.message}\n"
+      message += "#{e.backtrace}\n"
+
+      subscription.subscription_notifications.create!(body: message)
     end
   end
 
