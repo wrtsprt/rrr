96893a384b888856d0779276ee22fc8027a3c4b3
Raphael Doehring <raphael.doehring@vasco.com>
2017-02-21 13:46:55 +0100

    We need the url in case something goes wrong.

--- app/workers/refresh_subscription_worker.rb
+++ app/workers/refresh_subscription_worker.rb
@@ -10,6 +10,7 @@ class RefreshSubscriptionWorker
       Rails.logger.error e
 
       if subscription
+        url = subscription.feed_url
         message = "Error fetching for subscription #{subscription.id}\n"
         message += "#{subscription.name}: #{url}\n"
         message += "#{e.message}\n"
