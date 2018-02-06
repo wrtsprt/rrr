b408209bc9b647397ae712cb35797e917c4c483e
Raphael Doehring <raphael.doehring@gmail.com>
2018-02-06 20:40:58 +0100

    rake app:update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 
 // Loads all Bootstrap javascripts
-// //= require bootstrap
+//= require jquery3
+//= require popper
+//= require bootstrap
