d190502b2f1ed664b2b4c7c631b5a75449924329
Raphael Doehring <raphael.doehring@gmail.com>
2018-09-21 08:47:44 +0200

    Fixing vue warning (runtime only, template compiling)
    https://stackoverflow.com/questions/50473630/rails-webpacker-vue-you-are-using-the-runtime-only-build-of-vue-where-the
    
    You are using the runtime-only build of Vue where the template compiler is not available. Either pre-compile the templates into render functions, or use the compiler-included build.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 // app/javascript/packs/application.js
 
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm.js';
 import App from '../components/app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
