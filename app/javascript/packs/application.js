b23f11fa4d71a59694225ac38b0180409071bd4b
Raphael Doehring <raphael.doehring@gmail.com>
2018-09-21 09:08:59 +0200

    Put app element explicitly into view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import Vue from 'vue/dist/vue.esm.js';
 import App from '../components/app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.appendChild(document.createElement('app'))
 const app = new Vue({
     el: 'app',
     template: '<App/>',
